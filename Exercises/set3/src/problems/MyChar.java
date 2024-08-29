717e02492df06f68792de85687160ec49646eaf9
Ryan Moses <moses_ryan@bah.com>
2024-08-29 11:04:29 -0400

    Three exercises added in exercise package

---
+++ Exercises/set3/src/problems/MyChar.java
@@ -0,0 +1,15 @@
+package problems;
+
+public class MyChar {
+    // Method to determine if a character is a vowel
+    public boolean isVowel(char ch) {
+
+        // Use a switch statement to check for each vowel, both lowercase and uppercase
+        switch (ch) {
+
+            // TODO: Complete the switch statement
+        }
+
+        return false;
+    }
+}
---
+++ Exercises/set3/src/problems/WeatherAdviser.java
@@ -0,0 +1,8 @@
+package problems;
+
+public class WeatherAdviser {
+    public String provideWeatherAdvisory(int temperature) {
+        // TODO: Provide a weather advisory message based on the temperature.
+        return "";
+    }
+}
