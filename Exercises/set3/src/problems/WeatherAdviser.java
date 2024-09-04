aa2bcd1f96507ee916265509337a391d6c7d986e
Ryan Moses <moses_ryan@bah.com>
2024-09-04 09:10:05 -0400

    09-04-24 Problems

--- Exercises/set3/src/problems/WeatherAdviser.java
+++ Exercises/set3/src/problems/WeatherAdviser.java
@@ -3,6 +3,14 @@ package problems;
 public class WeatherAdviser {
     public String provideWeatherAdvisory(int temperature) {
         // TODO: Provide a weather advisory message based on the temperature.
-        return "";
+        if (temperature < 0) {
+            return "It's freezing! Wear a heavy coat.";
+        } else if (temperature <= 10) {
+            return "It's cold! Bundle up.";
+        } else if (temperature <= 20) {
+            return "It's cool! A light jacket will do.";
+        } else {
+            return "It's warm! Enjoy the day.";
+        }
     }
 }
